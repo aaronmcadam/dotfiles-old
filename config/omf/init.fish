aa9df410673b8431d7489783cab8a85d8f1c4368
Aaron McAdam <aaron@aaronmcadam.com>
2021-07-02 11:04:57 +0100

    upgrade Node to latest LTS

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -1,4 +1,7 @@
 set -gx P4PORT "10.1.207.6:1666"
+set -gx VISUAL nvim
+set -gx EDITOR nvim
 set -gx GIT_EDITOR nvim
 set -gx APPLE_CREDENTIALS_USR "ios2@lockwood-publishing.com"
 set -gx APPLE_CREDENTIALS_PSW "ascl-vqwz-jsae-lsrv"
+set -gx AWS_SDK_LOAD_CONFIG 1
