3f7a25ed84533ef0a557c40b1dbf518a146b3d00
Aaron McAdam <aaron@aaronmcadam.com>
2021-07-02 15:38:38 +0100

    Set up the very basics

---
+++ config/nvim/lua/utils/init.lua
@@ -0,0 +1,20 @@
+-- Copied from https://github.com/AceMouty/neovim/blob/master/lua/utils/init.lua
+local utils = { }
+
+local scopes = {o = vim.o, b = vim.bo, v = vim.v, w = vim.wo, g = vim.g}
+
+function utils.opt(scope, key, value)
+    if scopes[scope] then
+        scopes[scope][key] = value
+    else
+        vim.api.nvim_err_writeln(("%s is not a valid nvim scope"):format(scope))
+    end
+end
+
+function utils.map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+return utils
