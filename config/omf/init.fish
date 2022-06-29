746ae88c771394dc40051adf5c03078c0ef63955
Aaron McAdam <aaron.mcadam@ticketmaster.com>
2022-06-29 17:55:10 +0100

    configure lazygit

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -1,7 +1,6 @@
-set -gx P4PORT "10.1.207.6:1666"
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 set -gx GIT_EDITOR nvim
-set -gx APPLE_CREDENTIALS_USR "ios2@lockwood-publishing.com"
-set -gx APPLE_CREDENTIALS_PSW "ascl-vqwz-jsae-lsrv"
 set -gx AWS_SDK_LOAD_CONFIG 1
+# Configure lazy git configuration directory.
+set -gx CONFIG_DIR ~/.config/lazygit
