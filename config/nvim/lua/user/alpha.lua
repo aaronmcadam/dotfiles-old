16f30eb249373afff6e3fcdc386e2f1f4e89f634
Aaron McAdam <aaron.mcadam@ticketmaster.com>
2022-06-30 11:32:25 +0100

    remove unused option

--- config/nvim/lua/user/alpha.lua
+++ config/nvim/lua/user/alpha.lua
@@ -22,7 +22,6 @@ dashboard.section.buttons.val = {
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 
-dashboard.section.footer.opts.hl = "Type"
 dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
 
