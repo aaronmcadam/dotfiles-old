bf1b8dfb694b4fe80f801be5b6e301861bdbc856
Aaron McAdam <aaron.mcadam@ticketmaster.com>
2022-07-07 13:46:29 +0100

    switch theme to nightfox

--- config/nvim/lua/user/colorscheme.lua
+++ config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,4 @@
-local colorscheme = "tokyonight"
-
-vim.g.tokyonight_colors = { fg_gutter = "#707cb2", comment = "#709db2", dark5 = "#709db2" }
+local colorscheme = "duskfox"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -100,7 +100,7 @@ return packer.startup(function(use)
   -- use "nvim-neotest/neotest"
 
   -- Colorschemes
-  use "folke/tokyonight.nvim"
+  use "EdenEast/nightfox.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
